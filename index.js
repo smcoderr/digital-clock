dd8dbbeb3a4271e854276972bf271b2f469a2dc7
sourav-mm <souravoz2018@gmai.com>
2024-10-08 12:58:02 +0530

    second commit

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-setInterval(() => {
+setInterval(()=>{
   let currentTime = new Date();
-
   hrs.innerHTML =
     (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
   min.innerHTML =
     (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
   sec.innerHTML =
-    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSecond();
-}, 1000);
+    (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+},1000)
